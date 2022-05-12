1752b959d1e72b69f9f20270295f91b3465e28b8
Glenn Olsson <glennholsson@gmail.com>
2022-05-11 21:02:44 -0700

    Update TODOs

--- src/filesystem/file_coder.h
+++ src/filesystem/file_coder.h
@@ -6,15 +6,12 @@
 
 namespace FFS {
 
-	// TODO: Return list of Blob instead
 	std::vector<Magick::Blob*>* encode(std::string input_path);
-	// TODO: Return list of Blob instead
 	Magick::Blob* create_image(std::istream& input_stream, uint32_t length);
 	
-	// TODO: Take list of Blob instead
 	void decode(const std::vector<Magick::Blob*>* blobs, std::ostream& file_stream);
 
-
+	// TODO: DEPRECATED, remove and fix methods using them
 	void _save_encoded_image(std::string out_path, std::istream& file_stream, uint32_t length);
 	void _read_encoded_image(const std::string& path, std::ostream& file_stream);
 }
