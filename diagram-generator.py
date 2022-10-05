c92dbb9d63e67cce32f21509c51cf42260b413d5
Glenn Olsson <glennholsson@gmail.com>
2022-10-05 23:08:46 +0200

    Let diagram generator generate all 30 diagrams per log

--- diagram-generator.py
+++ diagram-generator.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import re
 from matplotlib import cm
-from matplotlib.ticker import LinearLocator
+from matplotlib.ticker import LinearLocator, ScalarFormatter
+
+fig_output_location = "figs/ffs"
 
 file_path = 'ffs.log'
 with open(file_path) as f:
@@ -22,27 +24,32 @@ report_pattern = r"\"(.+) report\""
 
 lines = file_content.splitlines()
 
-def generate_graph(name: str, x_vals: List[int], y_vals: List[int], z_vals: List[List[int]]):
+def generate_graphs(name: str, x_vals: List[int], y_vals: List[int], z_vals: List[List[int]]):
 	ax = plt.axes(projection='3d')
 
-	# Find max value of z list
-	m = 0
-	for l in z_vals:
-		for v in l:
-			if v > m:
-				m = v
+	fig = plt.figure(num=name, figsize=(16,10), dpi=180)
 
-	# Data for a three-dimensional line
-	# 0 to 110% of max of z values
-	zline = np.linspace(0, m*1.1)
-	xline = np.linspace(0, max(x_vals) * 1.1)
-	yline = np.linspace(0, max(y_vals) * 1.1)
-	ax.plot3D(xline, yline, zline, 'gray')
+	for fignr in range(5):
+		ax = fig.add_subplot(3, 2, fignr + 1)
 
-	# zdata = 15 * np.random.random(100)
-	# xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
-	# ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
-	ax.contour3D(x_vals, y_vals, z_vals, c=z_vals, cmap='Greens');
+		X = []
+		Y = []
+		Z = []
+		row = z_vals[fignr]
+		for j in range(len(row)):
+			Y.append(y_vals[fignr])
+			X.append(x_vals[j])
+			Z.append(row[j])
+
+		ax.set_xlabel('Record length, kB')
+		ax.set_xscale('log', base=2)
+		ax.set_xticks(x_vals)
+		ax.get_xaxis().set_major_formatter(ScalarFormatter())
+		ax.set_ylabel('Performance, kB/s')
+
+		ax.scatter(X, Z, color="black")
+	
+	fig.savefig(f"{fig_output_location}/{name}.pdf", bbox_inches='tight')
 
 def parse_report(name: str, index: int) -> int:
 	rec_lens_line = lines[index]
@@ -67,7 +74,7 @@ def parse_report(name: str, index: int) -> int:
 	print(len(values), len(values[-1]))
 	print(len(file_sizes), len(rec_lens))
 
-	generate_graph(name, rec_lens, file_sizes, values)
+	generate_graphs(name, rec_lens, file_sizes, values)
 
 	return i
 
@@ -84,37 +91,26 @@ def parse_file():
 
 		i += 1
 
-x_vals = [4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
-y_vals = [1024, 2048, 4096, 8192, 16384]
-z_vals = [
-	[71, 170, 169, 93, 77, 130, 74, 133, 157],
-	[183, 72, 150, 153, 216, 96, 67, 86, 32, 174],
-	[132, 105, 76, 112, 166, 131, 136, 87, 172, 101, 105],
-	[255, 337, 223, 275, 316, 193, 188, 253, 177, 169, 174, 239],
-	[320, 218, 288, 191, 291, 254, 363, 269, 340, 364, 317, 252, 208]
-]
+parse_file()
 
-X = []
-Y = []
-Z = []
-for i in range(2, len(z_vals)):
-	row = z_vals[i]
-	for j in range(len(row)):
-		Y.append(y_vals[i])
-		X.append(x_vals[j])
-		Z.append(row[j])
-	
-	break
+# x_vals = [4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384]
+# y_vals = [1024, 2048, 4096, 8192, 16384]
+# z_vals = [
+# 	[71, 170, 169, 93, 77, 130, 74, 133, 157],
+# 	[183, 72, 150, 153, 216, 96, 67, 86, 32, 174],
+# 	[132, 105, 76, 112, 166, 131, 136, 87, 172, 101, 105],
+# 	[255, 337, 223, 275, 316, 193, 188, 253, 177, 169, 174, 239],
+# 	[320, 218, 288, 191, 291, 254, 363, 269, 340, 364, 317, 252, 208]
+# ]
+# fig = plt.figure(num="Random write", figsize=(16,10), dpi=180)
+# generate_figure(0, fig)
+# generate_figure(1, fig)
+# generate_figure(2, fig)
+# generate_figure(3, fig)
+# generate_figure(4, fig)
 
-
-fig, ax = plt.subplots()
-
-ax.set_xlabel('Record length, kB')
-ax.set_xticks(x_vals)
-
-ax.set_ylabel('Performance, kB/s')
-
-surf = ax.scatter(X, Z, color="black")
+# fig.savefig(fig, bbox_inches='tight')
+# generate_figure(0)
 
 # fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
 
@@ -129,4 +125,4 @@ surf = ax.scatter(X, Z, color="black")
 # surf = ax.scatter3D(X, Y, Z, color="black")
 
 
-plt.show()
+# plt.show()
