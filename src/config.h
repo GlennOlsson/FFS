e5e9c60317426ac296c15e545f97da53875a4df5
Glenn Olsson <glennholsson@gmail.com>
2022-10-05 07:41:40 +0200

    New test run, gcsf didn't make it

--- src/config.h
+++ src/config.h
@@ -1,5 +1,5 @@
-// #define NO_LOGGING // Discards all logging
-#define DEBUG_LOGGING // Log to cout and cerr
+#define NO_LOGGING // Discards all logging
+// #define DEBUG_LOGGING // Log to cout and cerr
 // #define FILE_LOGGING // Log to log.log err.log
 
 // #define USE_LOCAL_STORAGE // Uses local storage for FFS images instead of OWS
